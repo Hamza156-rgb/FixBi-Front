55c987fa3b2f3e74fb252be2002d1e7e9ee7cac6
Hamza Waseem <69486753+Hamza156-rgb@users.noreply.github.com>
2022-08-30 16:09:14 +0500

    reviews

--- src/app/pages/provider/insight-provider/insight-provider.component.ts
+++ src/app/pages/provider/insight-provider/insight-provider.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/providers/config/config.service';
 
 @Component({
   selector: 'app-insight-provider',
@@ -6,10 +7,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./insight-provider.component.scss']
 })
 export class InsightProviderComponent implements OnInit {
+  name: any = '';
+  image: any = 'assets/img/download.png';
+  last: any = '';
+  email: any = '';
+  phone: any = '';
+  user_type: any = '';
+  id = localStorage.getItem("user_id");
+  jobs: any = '';
+  review: any = '';
 
-  constructor() { }
+
+  constructor(public config: ConfigService) { }
 
   ngOnInit(): void {
+    this.getUser(this.id);
+    this.getJobsNumber(this.id);
+    this.getReviews(this.id);
+  }
+
+  getUser(id: any) {
+
+    this.config.getHttp('auth/getSingleProfessionalById?id=' + id, { id: id }).then((data: any) => {
+      console.log(data);
+      this.last = data.data[0].user_data[0].last_name;
+      this.email = data.data[0].user_data[0].email;
+      this.phone = data.data[0].user_data[0].phone;
+      this.user_type = data.data[0].user_data[0].user_type;
+
+
+      if (data.data[0].user_data[0].user_type == 4) {
+        this.name = data.data[0].user_data[0].company_name;
+      }
+      else if (data.data[0].user_data[0].user_type == 3) {
+        this.name = data.data[0].user_data[0].first_name;
+        this.last = data.data[0].user_data[0].last_name;
+
+      }
+
+      if (data.data[0].user_data[0].profile_picture) {
+        this.image = this.config.imgUrl + data.data[0].user_data[0].profile_picture;
+      }
+    })
+  }
+
+  getJobsNumber(id: any) {
+    this.config.getHttp('auth/jobs/getInsightsProvider?user_id=' + id, { id: id }).then((data: any) => {
+      this.jobs = data.data;
+    })
+  }
+
+  getReviews(id: any) {
+    this.config.getHttp('auth/jobReview/getReviewsProvider?user_id=' + id, { id: id }).then((data: any) => {
+      this.review = data.data;
+    })
   }
 
 }
--- src/app/pages/user/insight-user/insight-user.component.ts
+++ src/app/pages/user/insight-user/insight-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/providers/config/config.service';
 
 @Component({
   selector: 'app-insight-user',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsightUserComponent implements OnInit {
 
-  constructor() { }
+  name: any = '';
+  last: any = '';
+  email: any = '';
+  phone:any='';
+  image: any = 'assets/img/download.png';
+  id = localStorage.getItem("user_id");
+  jobs: any = '';
+
+  constructor(public config: ConfigService) { }
 
   ngOnInit(): void {
+    this.getUser(this.id);
+    this.getJobsNumber(this.id)
+  }
+
+
+  getUser(id: any) {
+
+    this.config.getHttp('auth/getSingleCustomerById?id=' + id, { id: id }).then((data: any) => {
+      console.log(data);
+
+      this.name = data.user.first_name;
+      this.last = data.user.last_name;
+      this.email = data.user.email;
+      this.phone = data.user.phone;
+
+      if (data.user.profile_picture) {
+        this.image = this.config.imgUrl + data.user.profile_picture;
+      }
+    })
+  }
+
+  getJobsNumber(id: any) {
+    this.config.getHttp('auth/jobs/getInsightsUser?user_id=' + id, { id: id }).then((data: any) => {
+      console.log(data);
+      this.jobs = data.data;
+    })
   }
 
 }
